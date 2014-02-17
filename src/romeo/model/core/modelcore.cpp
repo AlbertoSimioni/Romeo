91c165b5cd8c728338e78ff62800e3d4c805436d
Federico Ziliotto <federico.ziliotto.1@studenti.unipd.it>
2014-02-17 17:22:14 +0100

    aggiunti file

---
+++ src/romeo/model/core/modelcore.cpp
@@ -0,0 +1,5 @@
+#include "modelcore.h"
+
+ModelCore::ModelCore()
+{
+}
---
+++ src/romeo/model/core/modelcore.h
@@ -0,0 +1,10 @@
+#ifndef MODELCORE_H
+#define MODELCORE_H
+
+class ModelCore
+{
+public:
+    ModelCore();
+};
+
+#endif // MODELCORE_H
