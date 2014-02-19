d5569bfce9cd33377939f79f8e0ce4bea2ad3af9
Federico Ziliotto <federico.ziliotto.1@studenti.unipd.it>
2014-02-19 16:34:24 +0100

    abstractdataset

--- src/romeo/model/datasets/abstractdataset.h
+++ src/romeo/model/datasets/abstractdataset.h
@@ -33,25 +33,25 @@ class AbstractDataset
 public:
     AbstractDataset();
     /*!
-     * \brief AbstractDataset
-     * \param name
+     * \brief Costruisce un nuovo dataset con il nome indicato
+     * \param name Nome con cui costruire in nuovo dataset
      */
     AbstractDataset(QString& name);
     /*!
-     * \brief createNewSubject
-     * \param n
-     * \param fileSubject
-     * \param mask
+     * \brief Metodo per la creazione di un nuovo subject che prende in input le informazioni necessarie alla creazione e richiama il metodo virtuale makeSubject() che viene ridefinito dalle sottoclassi e permette di creare i vari tipi di subjects.
+     * \param n Nome del subject da creare e aggiungere al dataset
+     * \param fileSubject Percorso del filesystem al file immagine del subject
+     * \param mask Percorso alla mask del subject
      */
     void createNewSubject(QString& n, QString& fileSubject, QString& mask);
     /*!
-     * \brief makeSubject
-     * \param n
-     * \param fileSubject
-     * \param mask
-     * \return
+     * \brief Metodo virtuale puro che deve essere ridefinito dalle sottoclassi in base al tipo di subject che si vuole costruire. Il metodo è chiamato da createNewSubject e costruisce un nuovo subject dello stesso tipo del tipo del dataset che ne ha richiesto la costruzione (2D per i dataset 2D, 3D per quelli 3D ecc..)
+     * \param n Nome del subject da creare
+     * \param fileSubject Percorso all'immagine associata al subject
+     * \param mask Percorso alla maschera associata al subject
+     * \return Ritorna un puntatore al nuovo subject appena creato
      */
-    AbstractSubject* makeSubject(QString& n, QString& fileSubject, QString& mask);
+    virtual AbstractSubject* makeSubject(QString& n, QString& fileSubject, QString& mask) = 0;
     /*!
      * \brief Associa un nuovo protocollo al dataset. Se il protocollo appartiene già al dataset, non viene modificato nulla e viene ritornato il valore false, altrimenti il protocollo viene aggiunto al dataset e viene ritornato true
      * \param protocol Il protocollo da aggiungere al dataset
@@ -66,11 +66,20 @@ public:
      * \brief Modifica il nome del dataset
      */
     void setName(QString &value);
+    /*!
+     * \brief Ritorna i subject collegati al dataset sotto forma di lista di QString
+     */
     QVector<QString> getSubjectList();
 
 
 private:
+    /*!
+     * \brief Nome del dataset.
+     */
     QString name;
+    /*!
+     * \brief Una hashmap che contiene i subject collegati al dataset e i risultati dei protocolli a loro legati
+     */
     QHash<AbstractSubject*, QVector<Result*> >  results;
 };
 }}}
