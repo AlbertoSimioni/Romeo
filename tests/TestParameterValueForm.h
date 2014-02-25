78eb6d629b64beb684ae137c36dc939e032829b3
Andrea Ballista Flandoli <andrea.ballist@gmail.com>
2014-02-25 15:04:16 +0100

    Modificati file relativi ai test

--- tests/TestParameterValueForm.h
+++ tests/TestParameterValueForm.h
@@ -9,14 +9,16 @@ using namespace romeo::view::dialogs;
 using namespace romeo::model::protocols::algorithms;
 using namespace romeo::model::protocols;
 
-class TestCheckValidity : public QObject
+class TestParameterValueForm : public QObject
 {
     Q_OBJECT
 
 public:
-    TestCheckValidity();
+    TestParameterValueForm();
 
 private slots:
-    void CheckValidity_data();
-    void CheckValidity();
+    void ParameterValueForm_data();
+    void ParameterValueForm();
+    //void CheckValidity_data();
+    //void CheckValidity();
 };
